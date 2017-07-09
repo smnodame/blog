66e90ccd494c45a65ffc08c0f81ff16db58a6643
smnodame <sometime.p@hotmail.com>
2017-07-08 21:14:32 -0700

    improve structure file

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,7 +1,108 @@
+<nav class="breadcrumb has-bullet-separator">
+    <ul>
+        <li><a>Bulma</a></li>
+        <li><a>Documentation</a></li>
+        <li><a>Components</a></li>
+        <li class="is-active"><a>Breadcrumb</a></li>
+    </ul>
+</nav>
+<h2 class="subtitle is-2">Subtitle 2</h2>
+<hr/>
+<div class="content is-small">
+    <p>Posted By Boonprakit Chaikaew</p>
+</div>
 
-<h1>Hello!</h1>
-<p>
-    <img src="/images/office-small.jpg" align="left" >
-    <a href="http://devbattles.com">DevBattles</a>
-    - this a cool site for programmers =)
-</p>
+<div class="notification">
+    This container is <strong>centered</strong> on desktop.
+</div>
+<div class="content">
+    <p>
+        Taylor Alison Swift (born December 13, 1989) is an American singer-songwriter. One of the most popular contemporary female recording artists, she is known for narrative songs about her personal life, which has received much media attention.
+        Born and raised in Pennsylvania, Swift moved to Nashville, Tennessee at age 14 to pursue a career in country music. She signed with the independent label Big Machine Records and became the youngest artist ever signed by the Sony/ATV Music publishing house. Her eponymous debut album in 2006 peaked at number five on Billboard 200 and spent the most weeks on the chart in the 2000s. The album's third single, "Our Song", made her the youngest person to single-handedly write and perform a number-one song on the Hot Country Songs chart. Swift's second album, Fearless, was released in 2008. Buoyed by the pop crossover success of the singles "Love Story" and "You Belong with Me", Fearless became the best-selling album of 2009 in the United States. The album won four Grammy Awards, with Swift becoming the youngest Album of the Year winner.
+        Swift was the sole writer of her 2010 album, Speak Now. It debuted at number one in the United States and the single "Mean" won two Grammy Awards. Her fourth album, Red (2012), yielded the successful singles "We Are Never Ever Getting Back Together" and "I Knew You Were Trouble". With her fifth album, the pop-focused 1989 (2014), she became the first act to have three albums sell a million copies within one week in the United States. Its singles "Shake It Off", "Blank Space", and "Bad Blood" reached number one in the US, Australia, and Canada. The album received three Grammy Awards, and Swift became the first woman and fifth act overall to win Album of the Year twice. The 2015 eponymous concert tour for 1989 became one of highest-grossing of the decade.
+        As a songwriter, Swift has received awards from the Nashville Songwriters Association and the Songwriters Hall of Fame. She is also the recipient of ten Grammy Awards, five Guinness World Records, one Emmy Award, 21 Billboard Music Awards, 11 Country Music Association Awards, eight Academy of Country Music Awards, and one Brit Award. Swift is one of the best-selling artists of all time, having sold more than 40 million albums—including 27.1 million in the US—and 130 million single downloads. She has appeared in Time's 100 most influential people in the world (2010 and 2015), Forbes' top-earning women in music (2011–2015), Forbes' 100 most powerful women (2015), and Forbes Celebrity 100 (2016). She was the youngest woman to be included in the third of these and ranked first in Celebrity 100.
+    </p>
+</div>
+<hr/>
+<article class="media">
+    <figure class="media-left">
+        <p class="image is-64x64">
+            <img src="http://bulma.io/images/placeholders/128x128.png">
+        </p>
+    </figure>
+    <div class="media-content">
+        <div class="content">
+            <p>
+                <strong>Barbara Middleton</strong>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta eros lacus, nec ultricies elit blandit non. Suspendisse pellentesque mauris sit amet dolor blandit rutrum. Nunc in tempus turpis.
+                <br>
+                <small><a>Like</a> · <a>Reply</a> · 3 hrs</small>
+            </p>
+        </div>
+
+        <article class="media">
+            <figure class="media-left">
+                <p class="image is-48x48">
+                    <img src="http://bulma.io/images/placeholders/96x96.png">
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <p>
+                        <strong>Sean Brown</strong>
+                        <br>
+                        Donec sollicitudin urna eget eros malesuada sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam blandit nisl a nulla sagittis, a lobortis leo feugiat.
+                        <br>
+                        <small><a>Like</a> · <a>Reply</a> · 2 hrs</small>
+                    </p>
+                </div>
+
+                <article class="media">
+                    Vivamus quis semper metus, non tincidunt dolor. Vivamus in mi eu lorem cursus ullamcorper sit amet nec massa.
+                </article>
+
+                <article class="media">
+                    Morbi vitae diam et purus tincidunt porttitor vel vitae augue. Praesent malesuada metus sed pharetra euismod. Cras tellus odio, tincidunt iaculis diam non, porta aliquet tortor.
+                </article>
+            </div>
+        </article>
+        <article class="media">
+            <figure class="media-left">
+                <p class="image is-48x48">
+                    <img src="http://bulma.io/images/placeholders/96x96.png">
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <p>
+                        <strong>Kayli Eunice </strong>
+                        <br>
+                        Sed convallis scelerisque mauris, non pulvinar nunc mattis vel. Maecenas varius felis sit amet magna vestibulum euismod malesuada cursus libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus lacinia non nisl id feugiat.
+                        <br>
+                        <small><a>Like</a> · <a>Reply</a> · 2 hrs</small>
+                    </p>
+                </div>
+            </div>
+        </article>
+    </div>
+</article>
+<article class="media">
+    <figure class="media-left">
+        <p class="image is-64x64">
+            <img src="http://bulma.io/images/placeholders/128x128.png">
+        </p>
+    </figure>
+    <div class="media-content">
+        <div class="field">
+            <p class="control">
+                <textarea class="textarea" placeholder="Add a comment..."></textarea>
+            </p>
+        </div>
+        <div class="field">
+            <p class="control">
+                <button class="button">Post comment</button>
+            </p>
+        </div>
+    </div>
+</article>
